549b3255ee5b727b20a5f0ebc5f6b4ff525f3743
LuciaMarchal <luciamrchl@gmail.com>
2024-04-16 21:05:30 +0200

    add kanban view

--- odoo/addons/project_mng/models/project_project.py
+++ odoo/addons/project_mng/models/project_project.py
@@ -10,7 +10,36 @@ class Project(models.Model):
     final_date = fields.Date()
     worker_ids = fields.One2many('project.employee', 'project_id', string='Workers')
     color = fields.Integer()
+    state = fields.Selection([
+        ('first_impressions', 'First impressions'),
+        ('to_do', 'To do'),
+        ('doing', 'Doing'),
+        ('testing', 'Testing'),
+        ('displayed', 'Displayed'),
+        ('done', 'Done'),
+        ('closed', 'Closed'),
+        ('canceled', 'Canceled')
+    ], default="first_impressions")
     
     task_ids = fields.One2many('project.task', 'project_id', string='tasks')
     document_ids = fields.One2many('project.document', 'project_id', string='Documents')
     report_ids = fields.One2many('project.report', 'project_id', string='Reports')
+    
+    def action_change_state(self):
+        for project in self:
+            if project.state == 'first_impressions':
+                project.state = 'to_do'
+            elif project.state ==  'to_do':
+                project.state = 'doing'
+            elif project.state ==  'doing':
+                project.state = 'testing'
+            elif project.state ==  'testing':
+                project.state = 'displayed'
+            elif project.state ==  'displayed':
+                project.state = 'done'
+            elif project.state == 'done':
+                project.state = 'closed'
+
+    def action_cancel(self):
+        for project in self:
+            project.state = "canceled"
