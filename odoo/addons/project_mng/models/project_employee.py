22104fe664e41276a40aca805201cff436a6fd58
LuciaMarchal <luciamrchl@gmail.com>
2024-04-17 20:02:30 +0200

    add funcion creacion de usuario

--- odoo/addons/project_mng/models/project_employee.py
+++ odoo/addons/project_mng/models/project_employee.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Employee(models.Model):
     _name = 'project.employee'
@@ -7,13 +7,36 @@ class Employee(models.Model):
     name = fields.Char()
     surnames = fields.Char()
     job_title = fields.Selection([
-        ('key', 'value')
+        ('manager', 'Project Manager'),
+        ('leader', 'Team Leader'),
+        ('developer', 'Developer'),
+        ('designer', 'Designer'),
+        ('tester', 'Tester'),
+        ('admin', 'Administrator'),
+        ('db_admin', 'Database Administrator'),
+        ('intern', 'Intern'),
     ])
     department = fields.Char()
     email = fields.Char()
     phone = fields.Char()
-    #usuario de odoo
+    user_id = fields.Many2one('res.users', string='User')
     
     project_id = fields.Many2one('project.project', string='Project')
     comment_ids = fields.One2many('project.comment', 'user_id', string='Comments')
     task_ids = fields.One2many('project.task', 'worker_id', string='Tasks')
+    
+    @api.model_create_single
+    def create(self, vals):
+        employee = super(Employee, self).create(vals)
+
+        user_vals = {
+            'name': employee.name,
+            'login': employee.email,
+            'password': 'contrasena', 
+            'groups_id': [(6, 0, [
+                self.env.ref('base.group_user').id
+            ])]
+        }
+        self.env['res.users'].create(user_vals)
+
+        return employee
