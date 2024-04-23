982633aa136811010d5e4509ce2cc0112d752ecf
LuciaMarchal <luciamrchl@gmail.com>
2024-04-23 20:00:43 +0200

    upd date field

--- odoo/addons/project_mng/models/project_message.py
+++ odoo/addons/project_mng/models/project_message.py
@@ -6,7 +6,7 @@ class Message(models.Model):
     _description = 'Message model'
 
     text = fields.Text()
-    date_time = fields.Datetime(default=datetime.now(), readonly=True)
+    date_time = fields.Datetime(default=fields.datetime.now(), readonly=True)
     user_id = fields.Many2one('res.users', string='User', default=lambda self: self.env.user.id, readonly=True)
     project_id = fields.Many2one('project.project', string='Project')
     
