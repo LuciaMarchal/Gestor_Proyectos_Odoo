e292cbdf5da3c9a1bf553c0aa039441deb88f3f9
LuciaMarchal <luciamrchl@gmail.com>
2024-04-28 19:33:27 +0200

    add sequence

--- odoo/addons/project_mng/models/project_report.py
+++ odoo/addons/project_mng/models/project_report.py
@@ -1,12 +1,19 @@
-from odoo import models, fields
+from odoo import models, fields, api
 from datetime import datetime
 
 class Report(models.Model):
     _name = 'project_mng.report'
     _description = 'Report model'
 
-    name = fields.Char()
+    name = fields.Char(readonly=True)
     description = fields.Text()
     file = fields.Binary()
     date = fields.Date(default=datetime.today(), readonly=True)
     project_id = fields.Many2one('project_mng.project', string='Project')
+    
+    @api.model
+    def create(self, vals):
+        vals['name'] = self.env['ir.sequence'].next_by_code('report.code')
+        result = super(Report, self).create(vals)
+        return result
+
