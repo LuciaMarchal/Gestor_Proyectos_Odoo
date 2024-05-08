0703a3aecdcf37de88fc40af3eb3420aa4a01d87
LuciaMarchal <luciamrchl@gmail.com>
2024-05-08 20:50:40 +0200

    add campo name

--- odoo/addons/project_mng/models/project_resource.py
+++ odoo/addons/project_mng/models/project_resource.py
@@ -4,6 +4,7 @@ class Resource(models.Model):
     _name = 'project_mng.resource'
     _description = 'Resource model'
     
+    name = fields.Char(compute='_compute_name', store=True, readonly=True)
     description = fields.Text()
     type = fields.Selection([
         ('human_resources', 'Human Resources'),
@@ -12,3 +13,7 @@ class Resource(models.Model):
     ])
     availability = fields.Integer()
     
+    def _compute_name(self):
+        for resource in self:
+            if resource.type:
+                resource.name = dict(self._fields['type'].selection).get(resource.type)
