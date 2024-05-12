16dc3a077e608f74ac63d846f8423727edcedb50
LuciaMarchal <luciamrchl@gmail.com>
2024-05-12 16:29:44 +0200

    upd compute

--- odoo/addons/project_time_tracking/models/project_project.py
+++ odoo/addons/project_time_tracking/models/project_project.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class Project(models.Model):
     _inherit = 'project_mng.project'
 
-    total_duration = fields.Float(compute='_compute_total_duration')
+    total_duration = fields.Float(compute='_compute_total_duration', store=True)
     
     def _compute_total_duration(self):
         for record in self:
