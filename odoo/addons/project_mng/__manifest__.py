ad5faf589a7fe7aadcbd9b9d23eae58d786f2967
LuciaMarchal <luciamrchl@gmail.com>
2024-05-12 13:13:02 +0200

    upd

--- odoo/addons/project_mng/__manifest__.py
+++ odoo/addons/project_mng/__manifest__.py
@@ -13,16 +13,11 @@
     'author': "Lucia Marchal",
     'website': "https://www.luciamarchal.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Other',
     'version': '16.0',
 
-    # any module necessary for this one to work correctly
     'depends': ['base'],
 
-    # always loaded
     'data': [
         'data/demo.xml',
         'security/ir.model.access.csv',
@@ -33,7 +28,7 @@
         'views/actions.xml',
         'views/menus.xml',
     ],
-    # only loaded in demonstration mode
+
     'demo': [
         'data/demo.xml',
     ],
