0685853ad7a742f2a9280a46f5b760467795a543
LuciaMarchal <luciamrchl@gmail.com>
2024-05-15 14:42:51 +0200

    del compute field

--- odoo/addons/project_mng/models/project_resource.py
+++ odoo/addons/project_mng/models/project_resource.py
@@ -4,7 +4,6 @@ class Resource(models.Model):
     _name = 'project_mng.resource'
     _description = 'Resource model'
     
-    name = fields.Char(compute='_compute_name', store=True, readonly=True)
     description = fields.Text()
     type = fields.Selection([
         ('human_resources', 'Human Resources'),
@@ -14,7 +13,3 @@ class Resource(models.Model):
     ], required=True)
     availability = fields.Integer(required=True)
     
-    def _compute_name(self):
-        for resource in self:
-            if resource.type:
-                resource.name = dict(self._fields['type'].selection).get(resource.type)
